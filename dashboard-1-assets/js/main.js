00415e579085cdaedcd831810f52f8cd7aa31526
Tom Ainsworth <tomainsworth94@gmail.com>
2022-08-25 18:34:17 +0100

    Add boilerplate code

---
+++ dashboard-1-assets/js/main.js
@@ -0,0 +1,22 @@
+// Get CSS Variables
+const colorPrimary = getComputedStyle(document.documentElement)
+	.getPropertyValue('--color-primary')
+	.trim();
+
+const colorAccent = getComputedStyle(document.documentElement)
+	.getPropertyValue('--color-accent')
+	.trim();
+
+// Declare Default Chart Options
+
+// Create sparklines bar chart
+
+// Create radial bar chart
+
+// Create bar Chart
+
+// Create gauge Chart
+
+// Create area chart
+
+// Create line Chart
